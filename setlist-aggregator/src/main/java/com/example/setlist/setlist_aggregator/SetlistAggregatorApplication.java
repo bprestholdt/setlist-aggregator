4eb02159dea89efb2f40d8b3acdbdffbbbface84
Braden <braden.prestholdt@uwrf.edu>
2024-10-15 02:39:52 -0500

    initial commit of spring boot project structure

---
+++ setlist-aggregator/src/main/java/com/example/setlist/setlist_aggregator/SetlistAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.setlist.setlist_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetlistAggregatorApplication.class, args);
+	}
+
+}
---
+++ setlist-aggregator/src/test/java/com/example/setlist/setlist_aggregator/SetlistAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.setlist.setlist_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
